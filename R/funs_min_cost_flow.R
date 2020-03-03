5f3fb74ed62c6047d5dbbc9556bc8405699bf90f
Laurens Geffert <laurensgeffert@gmail.com>
2020-03-03 14:33:28 -0500

    starting work on edge creation function

--- R/funs_min_cost_flow.R
+++ R/funs_min_cost_flow.R
@@ -39,6 +39,134 @@ get_dummy_data <- function() {
 }
 
 
+
+create_edge_df <- function(
+  layers_habitat,
+  layer_cost
+) {
+  # TODO: some defensive programming here:
+  # check inputs and give informative error messages if unexpected format
+
+  # exclude cells with NA cost from habitat layers
+  # (these are not available for use in the final solution)
+  layers_habitat <- layers_habitat * !is.na(layer_cost)
+
+  # convert raster layers into data frames -------------------------------------
+  df_habitat <- map_dfr(
+    layers_habitat = layers_habitat,
+    .x = 1:nlayers(layers_habitat),
+    .f = ~layers_habitat[[.x]] %>%
+      values() %>%
+      tibble(habitat = .) %>%
+      transmute(
+        cell_id = row_number(),
+        time = .x,
+        habitat = habitat)) %>%
+    # give the same cell in different time slices different IDs
+    mutate(raster_id = cell_id, cell_id = cell_id + max(cell_id) * time)
+
+  df_ids <- df_habitat %>%
+    rename(node_to = cell_id) %>%
+    dplyr::select(node_to, raster_id)
+
+  df_cost <- layer_cost %>%
+    values() %>%
+    tibble(cost = .) %>%
+    mutate(cell_id = row_number())
+
+  df_cost <- map_dfr(
+    df_cost = df_cost,
+    .x = 1:nlayers(layers_habitat),
+    .f = ~df_cost %>%
+      transmute(
+        cell_id = cell_id + max(cell_id) * .x,
+        edge_cost = cost))
+
+  # find valid edges  ----------------------------------------------------------
+  connections <- edge_distance_limit(
+    layer_cost = layer_cost,
+    layers_habitat = layers_habitat,
+    Dist = Dist)
+
+  edges_timesteps <- map_dfr(
+    df_habitat = df_habitat,
+    .x = 2:nlayers(layers_habitat),
+    .f = ~expand_grid(
+      node_from = df_habitat %>%
+        filter(time == .x - 1 & habitat == 1) %>%
+        pull(cell_id),
+      node_to = df_habitat %>%
+        filter(time == .x & habitat == 1) %>%
+        pull(cell_id),
+      timeslice_to = .x)) %>% mutate(
+        edge_id = row_number(),
+        edge_capacity = 1,
+        edge_cost = NA)
+
+  # double cost hack
+  for (i in 1:nrow(edges_timesteps)) {
+    # check if the raster ID is equal in node_to and node_from
+    if (df_IDs$raster_id[df_IDs$node_to == edges_timesteps[i, ]$node_to] == df_IDs$raster_id[df_IDs$node_to == edges_timesteps[i, ]$node_from]) {
+      # if that is the case just add the cost once
+      edges_timesteps$edge_cost[i] <- df_cost$edge_cost[df_cost$cell_id == edges_timesteps[i, ]$node_from]
+    }
+    # check if the raster ID is Different in node_to and node_from
+    if (df_IDs$raster_id[df_IDs$node_to == edges_timesteps[i, ]$node_to] != df_IDs$raster_id[df_IDs$node_to == edges_timesteps[i, ]$node_from]) {
+      # if that is the case just add the cost twice
+      edges_timesteps$edge_cost[i] <- df_cost$edge_cost[df_cost$cell_id == edges_timesteps[i, ]$node_from] + df_cost$edge_cost[df_cost$cell_id == edges_timesteps[i,]$node_to]
+    }
+  }
+
+  edges_timesteps <- edges_timesteps %>%
+    left_join(connections) %>%
+    dplyr::filter(!is.na(dist)) %>%
+    dplyr::select(-dist)
+
+  # add edges for source and target node  --------------------------------------
+  edges_source <- tibble(
+    # lowest ids available
+    # FIXME: temp workaround
+    edge_id = min(edges_timesteps[['edge_id']]),
+    node_from = 0,
+    # find all timeslice 1 nodes
+    node_to = edges_timesteps %>%
+      filter(timeslice_to == 2) %>%
+      pull(node_from) %>%
+      unique(),
+    timeslice_to = 1,
+    edge_capacity = nchains,
+    edge_cost = 1) %>%
+    # unique ids!
+    mutate(edge_id = edge_id - row_number())
+
+  edges_target <- tibble(
+    # FIXME: temp workaround
+    edge_id = max(edges_timesteps[['edge_id']]),
+    node_from = edges_timesteps %>%
+      filter(timeslice_to == max(edges_timesteps[['timeslice_to']])) %>%
+      pull(node_to) %>%
+      unique(),
+    node_to = max(edges_timesteps[['node_to']]) + 1,
+    timeslice_to = max(edges_timesteps[['timeslice_to']]) + 1,
+    edge_capacity = nchains,
+    edge_cost = 1) %>%
+    # unique ids!
+    mutate(edge_id = edge_id + row_number())
+
+  edges_formatted <- edges_timesteps %>%
+    bind_rows(edges_source, edges_target) %>%
+    arrange(edge_id)
+
+  return(edges_formatted)
+}
+
+
+
+
+
+
+
+
 data_phillips_problem <- function() {
   #' helper function to re-create phillips problem in edge list format
   edges_phillips <- data.frame(
